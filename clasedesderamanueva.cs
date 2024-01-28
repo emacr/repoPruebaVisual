8dd3302c25dec3d0c1ad11729e99498c2fba34d1
emacr <manuelcarranzaw@gmail.com>
2024-01-28 17:12:45 -0600

    creo ramanueva para poner cambios

---
+++ clasedesderamanueva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba1
+{
+    internal class clasedesderamanueva
+    {
+        static void Main(string[] args)
+        {
+            // Pedir el nombre de usuario
+            Console.WriteLine("esta es una clase desde una nueva rama");
+            string nombre = Console.ReadLine();
+        }
+    }
+}
